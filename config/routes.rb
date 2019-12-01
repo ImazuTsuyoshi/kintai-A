49f353f75769415ff2f992795f4296be22260f9a
Imazu Tsuyoshi <ihsoyust0907@icloud.com>
2019-12-01 00:59:52 +0000

    Change from HTTPS to SSH

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
+  root 'static_pages#top'
+  get '/signup', to: 'users#new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users do
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+      get 'attendances/edit_one_month'
+      patch 'attendances/update_one_month' 
+    end
+    resources :attendances, only: :update
+  end
 end
